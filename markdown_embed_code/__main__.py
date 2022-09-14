ea9c3c53c5e3ad89bcddd5e409c1e6c116847810
Randy Jimenez <randy.jimenez@nyu.edu>
2022-09-14 12:35:43 -0400

    Refactor main code.

--- markdown_embed_code/__main__.py
+++ markdown_embed_code/__main__.py
@@ -23,12 +23,14 @@ class Settings(BaseSettings):
 
 
 def run_command(command: List, **kwargs):
-    return subprocess.run(
+    completed_process = subprocess.run(
         command,
         check=True,
         **kwargs,
     )
 
+    completed_process.check_returncode()
+    return completed_process
 
 def overwrite_file(file_handle, new_contents):
     file_handle.seek(0)
@@ -36,34 +38,31 @@ def overwrite_file(file_handle, new_contents):
     file_handle.write(new_contents)
 
 
-settings = Settings()
+try:
+    settings = Settings()
 
-run_command(["git", "config", "--local", "user.name", "github-actions"])
-run_command(["git", "config", "--local", "user.email", "github-actions@github.com"])
+    run_command(["git", "config", "--local", "user.name", "github-actions"])
+    run_command(["git", "config", "--local", "user.email", "github-actions@github.com"])
 
-ref = settings.github_head_ref or settings.github_ref
+    ref = settings.github_head_ref or settings.github_ref
 
-if ref is None:
-    sys.exit(1)
+    if not ref:
+        sys.exit(1)
 
-for path in Path(".").glob(str(settings.input_markdown)):
-    with open(path, "r+") as f:
-        overwrite_file(f, convert(f.read()))
-        run_command(["git", "add", path])
+    for path in Path(".").glob(str(settings.input_markdown)):
+        with open(path, "r+") as f:
+            overwrite_file(f, convert(f.read()))
+            run_command(["git", "add", path])
 
-completed_process = run_command(
-    ["git", "status", "--porcelain"],
-    stdout=subprocess.PIPE,
-)
+    git_status_output = run_command(
+        ["git", "status", "--porcelain"],
+        stdout=subprocess.PIPE,
+    ).stdout
 
-# No changes.
-if not completed_process.stdout:
-    sys.exit(0)
+    if git_status_output:
+        run_command(["git", "commit", "-m", settings.input_message])
+        remote_repo = f"https://{settings.github_actor}:{settings.input_token.get_secret_value()}@github.com/{settings.github_repository}.git"
+        run_command(["git", "push", remote_repo, f"HEAD:{ref}"])
 
-run_command(["git", "commit", "-m", settings.input_message])
-
-remote_repo = f"https://{settings.github_actor}:{settings.input_token.get_secret_value()}@github.com/{settings.github_repository}.git"
-completed_process = run_command(["git", "push", remote_repo, f"HEAD:{ref}"])
-
-if completed_process.returncode != 0:
+except subprocess.CalledProcessError:
     sys.exit(1)

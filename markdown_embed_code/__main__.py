65eb1e1e9bd0c50591523f2bcf49dfabfb6719ec
Randy Jimenez <randy.jimenez@nyu.edu>
2022-09-14 16:20:06 -0400

    Having explicit try / except block was superfluous.

--- markdown_embed_code/__main__.py
+++ markdown_embed_code/__main__.py
@@ -33,37 +33,32 @@ def overwrite_file(file_handle, new_contents):
     file_handle.truncate()
 
 
-try:
-    settings = Settings()
+settings = Settings()
 
-    run_command(["git", "config", "--local", "user.name", "github-actions"])
-    run_command(["git", "config", "--local", "user.email", "github-actions@github.com"])
+run_command(["git", "config", "--local", "user.name", "github-actions"])
+run_command(["git", "config", "--local", "user.email", "github-actions@github.com"])
 
-    ref = settings.github_head_ref or settings.github_ref
+ref = settings.github_head_ref or settings.github_ref
 
-    if not ref:
-        sys.exit(1)
-
-    if Path(settings.input_markdown).is_dir():
-        settings.input_markdown = f'{settings.input_markdown}/*.md'
-
-    for path in Path(".").glob(settings.input_markdown):
-        with open(path, "r+") as f:
-            overwrite_file(f, convert(f.read()))
-            run_command(["git", "add", path])
-
-    git_status_output = run_command(
-        ["git", "status", "--porcelain"],
-        stdout=subprocess.PIPE,
-    ).stdout
-
-    if git_status_output:
-        run_command(["git", "commit", "-m", settings.input_message])
-        remote_repo = f"https://{settings.github_actor}:{settings.input_token.get_secret_value()}@github.com/{settings.github_repository}.git"
-        run_command(["git", "push", remote_repo, f"HEAD:{ref}"])
-    else:
-        print("No changes were made.")
-
-except subprocess.CalledProcessError as e:
-    print(e)
+if not ref:
     sys.exit(1)
+
+if Path(settings.input_markdown).is_dir():
+    settings.input_markdown = f'{settings.input_markdown}/*.md'
+
+for path in Path(".").glob(settings.input_markdown):
+    with open(path, "r+") as f:
+        overwrite_file(f, convert(f.read()))
+        run_command(["git", "add", path])
+
+git_status_output = run_command(
+    ["git", "status", "--porcelain"],
+    stdout=subprocess.PIPE,
+).stdout
+
+if git_status_output:
+    run_command(["git", "commit", "-m", settings.input_message])
+    remote_repo = f"https://{settings.github_actor}:{settings.input_token.get_secret_value()}@github.com/{settings.github_repository}.git"
+    run_command(["git", "push", remote_repo, f"HEAD:{ref}"])
+else:
+    print("No changes were made.")

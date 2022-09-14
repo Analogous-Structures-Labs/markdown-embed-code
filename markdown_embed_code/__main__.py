7a0589c7cc0c96d681ae23630d9057e8a81e59bb
Randy Jimenez <randy.jimenez@nyu.edu>
2022-09-13 23:29:36 -0400

    Add more debug.

--- markdown_embed_code/__main__.py
+++ markdown_embed_code/__main__.py
@@ -1,7 +1,7 @@
 import subprocess
 import sys
 from pathlib import Path
-from typing import Optional
+from typing import List, Optional
 
 from github import Github
 from pydantic import BaseModel, BaseSettings, SecretStr
@@ -30,27 +30,25 @@ class PartialGitHubEvent(BaseModel):
     inputs: Optional[PartialGitHubEventInputs] = None
 
 
+def run_command(command: List, **kwargs):
+    subprocess.run(
+        command,
+        check=True,
+        **kwargs,
+    )
+
+
 settings = Settings()
 
-default_subprocess_args = {
-    'check': True,
-}
+run_command(["git", "config", "--local", "user.name", "github-actions"])
+run_command(["git", "config", "--local", "user.email", "github-actions@github.com"])
 
-subprocess.run(
-    ["/usr/bin/git", "config", "--local", "user.name", "github-actions"],
-    **default_subprocess_args,
-)
-subprocess.run(
-    ["/usr/bin/git", "config", "--local", "user.email", "github-actions@github.com"],
-    **default_subprocess_args,
-)
-
-g = Github(settings.input_token.get_secret_value())
-repo = g.get_repo(settings.github_repository)
+repo = Github(settings.input_token.get_secret_value()).get_repo(settings.github_repository)
 
 if not settings.github_event_path.is_file():
     sys.exit(1)
 contents = settings.github_event_path.read_text()
+print(contents)
 event = PartialGitHubEvent.parse_raw(contents)
 
 number = None
@@ -62,9 +60,6 @@ elif event.inputs and event.inputs.number:
 # Ignore already merged PRs.
 if number and repo.get_pull(number).merged:
     sys.exit(0)
-
-print("loop time")
-print(settings.input_markdown)
 
 for path in Path(".").glob(str(settings.input_markdown)):
     with open(path, "r+") as f:
@@ -79,15 +74,11 @@ for path in Path(".").glob(str(settings.input_markdown)):
         if settings.input_output.is_dir():
             output_path = f'{settings.input_output}/{path}'
 
-        subprocess.run(
-            ["/usr/bin/git", "add", output_path],
-            **default_subprocess_args,
-        )
+        run_command(["git", "add", output_path])
 
-proc = subprocess.run(
-    ["/usr/bin/git", "status", "--porcelain"],
+proc = run_command(
+    ["git", "status", "--porcelain"],
     stdout=subprocess.PIPE,
-    **default_subprocess_args,
 )
 
 if not proc.stdout:
@@ -96,16 +87,10 @@ if not proc.stdout:
         pr.create_issue_comment(settings.input_no_change)
     sys.exit(0)
 
-subprocess.run(
-    ["/usr/bin/git", "commit", "-m", settings.input_message],
-    **default_subprocess_args,
-)
+run_command(["git", "commit", "-m", settings.input_message])
 
 remote_repo = f"https://{settings.github_actor}:{settings.input_token.get_secret_value()}@github.com/{settings.github_repository}.git"
-proc = subprocess.run(
-    ["/usr/bin/git", "push", remote_repo, f"HEAD:{pr.head.ref}"],
-    **default_subprocess_args,
-)
+proc = run_command(["git", "push", remote_repo, f"HEAD:{pr.head.ref}"])
 
 if proc.returncode != 0:
     sys.exit(1)

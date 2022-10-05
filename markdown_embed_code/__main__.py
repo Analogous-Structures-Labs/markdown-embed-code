311d5e19824d8c5f7ead4656931cc7a267ec3f25
Randy Jimenez <randy.jimenez@nyu.edu>
2022-10-05 01:13:29 -0400

    Clean up use of workspace.

--- markdown_embed_code/__main__.py
+++ markdown_embed_code/__main__.py
@@ -14,6 +14,7 @@ class Settings(BaseSettings):
     github_head_ref: str
     github_ref: str
     github_repository: str
+    github_workspace: Path
     input_markdown: str = "README.md"
     input_message: str = "Embed code into Markdown."
     input_token: SecretStr
@@ -32,23 +33,25 @@ ref = settings.github_head_ref or settings.github_ref
 if not ref:
     exit(1)
 
+workspace = Path(settings.github_workspace)
+
 # WORKAROUND: The checkout action clones the repo out as the runner user (id 1001) and our
 # container / script runs as root, as recommended by the actions documentation.
 # The below ensures that this script has permission to do its work.
 run(
-    f"chown -R $(id -u) .",
+    f"chown -R $(id -u) {workspace}",
     check=True,
     shell=True,
 )
 
-repo = Repo(".")
+repo = Repo(workspace)
 repo.remotes.origin.set_url(
     f"https://{settings.github_actor}:{settings.input_token.get_secret_value()}@github.com/{settings.github_repository}.git"
 )
 
-markdown_glob = f'{settings.input_markdown}/*.md' if Path(settings.input_markdown).is_dir() else settings.input_markdown
+markdown_glob = f"{settings.input_markdown}/*.md" if (workspace / settings.input_markdown).is_dir() else settings.input_markdown
 
-for file_path in Path(".").glob(markdown_glob):
+for file_path in Path(workspace).glob(markdown_glob):
     with file_path.open("r+") as file:
         overwrite_file(file, render_markdown(file.read()))
         repo.index.add([str(file_path)])

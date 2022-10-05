c567a047e73d7fa256709a43a9ec3ff91c70246f
Randy Jimenez <randy.jimenez@nyu.edu>
2022-10-05 01:30:08 -0400

    Gate main module code.

--- markdown_embed_code/__main__.py
+++ markdown_embed_code/__main__.py
@@ -23,51 +23,57 @@ class Settings(BaseSettings):
     input_token: SecretStr
 
 
-def overwrite_file(file_handle: TextIO, new_contents: str):
+def overwrite_file(
+    file_handle: TextIO,
+    new_contents: str,
+):
     file_handle.seek(0)
     file_handle.write(new_contents)
     file_handle.truncate()
 
 
-settings = Settings()
+def main(settings: Settings):
+    ref = settings.github_head_ref or settings.github_ref
 
-ref = settings.github_head_ref or settings.github_ref
+    if not ref:
+        raise MissingRefError()
 
-if not ref:
-    exit(1)
+    workspace = Path(settings.github_workspace)
 
-workspace = Path(settings.github_workspace)
-
-# WORKAROUND: The checkout action clones the repo out as the runner user (id 1001) and our
-# container / script runs as root, as recommended by the actions documentation.
-# The below ensures that this script has permission to do its work.
-run(
-    f"chown -R $(id -u) {workspace}",
-    check=True,
-    shell=True,
-)
-
-repo = Repo(workspace)
-repo.remotes.origin.set_url(
-    f"https://{settings.github_actor}:{settings.input_token.get_secret_value()}@github.com/{settings.github_repository}.git"
-)
-
-markdown_glob = f"{settings.input_markdown}/*.md" if (workspace / settings.input_markdown).is_dir() else settings.input_markdown
-
-for file_path in Path(workspace).glob(markdown_glob):
-    with file_path.open("r+") as file:
-        overwrite_file(file, render_markdown(file.read()))
-        repo.index.add([str(file_path)])
-
-if repo.is_dirty(untracked_files=True):
-    repo.index.commit(
-        settings.input_message,
-        author=Actor(
-            name=settings.github_actor,
-            email=f"{settings.github_actor}@github.com",
-        ),
-    raise MissingRefError()
+    # WORKAROUND: The checkout action clones the repo out as the runner user (id 1001) and our
+    # container / script runs as root, as recommended by the actions documentation.
+    # The below ensures that this script has permission to do its work.
+    run(
+        f"chown -R $(id -u) {workspace}",
+        check=True,
+        shell=True,
     )
-    repo.remotes.origin.push(f"HEAD:{ref}").raise_if_error()
-else:
-    print("No changes to commit.")
+
+    repo = Repo(workspace)
+    repo.remotes.origin.set_url(
+        f"https://{settings.github_actor}:{settings.input_token.get_secret_value()}@github.com/{settings.github_repository}.git"
+    )
+
+    markdown_glob = f"{settings.input_markdown}/*.md" if (workspace / settings.input_markdown).is_dir() else settings.input_markdown
+
+    for file_path in Path(workspace).glob(markdown_glob):
+        with file_path.open("r+") as file:
+            overwrite_file(file, render_markdown(file.read()))
+            repo.index.add([str(file_path)])
+
+    if repo.is_dirty(untracked_files=True):
+        repo.index.commit(
+            settings.input_message,
+            author=Actor(
+                name=settings.github_actor,
+                email=f"{settings.github_actor}@github.com",
+            ),
+        )
+        repo.remotes.origin.push(f"HEAD:{ref}").raise_if_error()
+    else:
+        print("No changes to commit.")
+
+
+if __name__ == "__main__":
+    settings = Settings()
+    main(settings)

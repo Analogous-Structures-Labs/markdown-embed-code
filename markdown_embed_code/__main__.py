5f5bcc231dc51d98f39150e378f04b75733e5d37
Randy Jimenez <randy.jimenez@nyu.edu>
2022-10-04 23:47:58 -0400

    Move commands around a bit.

--- markdown_embed_code/__main__.py
+++ markdown_embed_code/__main__.py
@@ -14,18 +14,9 @@ class Settings(BaseSettings):
     github_head_ref: str
     github_ref: str
     github_repository: str
-    github_workspace: str
     input_markdown: str = "README.md"
     input_message: str = "Embed code into Markdown."
     input_token: SecretStr
-
-
-def run_command(command: List, **kwargs):
-    return run(
-        command,
-        check=True,
-        **kwargs,
-    )
 
 
 def overwrite_file(file_handle, new_contents):
@@ -36,15 +27,19 @@ def overwrite_file(file_handle, new_contents):
 
 settings = Settings()
 
-# The checkout action checks out code as the runner user (1001:121). Our docker image runs as root
-# as recommended by the GitHub actions documentation. For that reason, we're ensuring he the user
-# running the script owns the workspace. Otherwise, the subsequent git commands will fail.
-run_command(f"chown -R $(id -u):$(id-g) {settings.github_workspace}", shell=True)
-
 ref = settings.github_head_ref or settings.github_ref
 
 if not ref:
     exit(1)
+
+# WORKAROUND: The checkout action checks out code as the runner user (1001:121). Our container
+# runs as root as recommended by the GitHub actions documentation. The subsequent git commands fail.
+# The below ensures that the runner of this script owns the repo directory.
+run(
+    f"chown -R $(id -u):$(id-g) .",
+    check=True,
+    shell=True,
+)
 
 actor = Actor(settings.github_actor, "github-actions@github.com")
 remote_repo_url = f"https://{settings.github_actor}:{settings.input_token.get_secret_value()}@github.com/{settings.github_repository}.git"

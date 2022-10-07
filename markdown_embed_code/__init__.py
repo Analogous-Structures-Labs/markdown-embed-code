c52735c3bcf68ff03b1794a5dc275e396bc959a8
Randy Jimenez <randy.jimenez@nyu.edu>
2022-10-07 17:22:42 -0400

    More closely trim down code for setting start and end.

--- markdown_embed_code/__init__.py
+++ markdown_embed_code/__init__.py
@@ -20,18 +20,17 @@ class Embed(Iterable):
     @classmethod
     def parse_from_extra(cls, extra: str) -> Embed:
         try:
-            pattern = r"\s*(?P<file_path>.+\S)(?:\s*\[\s*(?P<start_at>\d*)\s*(?P<colon>:)?\s*(?P<end_at>\d*)?\s*\])"
-            file_path, start_at, colon, end_at = match(pattern, extra).group("file_path", "start_at", "colon", "end_at")
+            pattern = r"\s*(?P<file_path>.+\S)(?:\s*\[\s*(?P<start_at>\d*)\s*(?P<has_colon>:)?\s*(?P<end_at>\d*)?\s*\])"
+            file_path, start_at, has_colon, end_at = match(pattern, extra).group("file_path", "start_at", "has_colon", "end_at")
+            end_at = start_at if start_at and not has_colon else end_at
+            end_at = None if has_colon and not end_at else end_at
         except AttributeError:
-            file_path, start_at, colon, end_at = extra, 1, None, None
-
-        start_at = int(start_at or 1) or 1
-        end_at = start_at if not colon else (int(end_at) if end_at else None)
+            file_path, start_at, end_at = extra, 1, None
 
         return cls(
             file_path=Path(file_path),
-            start_at=start_at,
-            end_at=end_at,
+            start_at=int(start_at or 1) or 1,
+            end_at=int(end_at) or 1 if end_at else None,
         )
 
     def __iter__(self):
@@ -61,13 +60,9 @@ class MarkdownEmbedCodeRenderer(MarkdownRenderer):
 _markdown = Markdown(renderer=MarkdownEmbedCodeRenderer)
 
 
-def render_markdown(document: str) -> str:
-    return _markdown(document)
-
-
 def render_markdown_file(file_path: Path):
     with file_path.open("r+") as file:
-        rendered_contents = render_markdown(file.read())
+        rendered_contents = _markdown(file.read())
         file.seek(0)
         file.write(rendered_contents)
         file.truncate()

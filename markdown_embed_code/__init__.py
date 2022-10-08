488123f2e5aeb20cdc6e849735e15d561ea750a7
Randy Jimenez <randy.jimenez@nyu.edu>
2022-10-08 12:47:27 -0400

    Mimic python slicing syntax. (#10)

--- markdown_embed_code/__init__.py
+++ markdown_embed_code/__init__.py
@@ -1,27 +1,18 @@
 from __future__ import annotations
 
+from collections.abc import Iterable
 from dataclasses import dataclass
 from itertools import islice
 from pathlib import Path
 from re import match
-from typing import Iterator, Optional
+from typing import Optional
 
 from marko import Markdown
 from marko.md_renderer import MarkdownRenderer
 
 
-def file_slice(
-    file_path: Path,
-    start_at: int = 1,
-    end_at: Optional[int] = None,
-) -> Iterator[str]:
-    with file_path.open() as file:
-        for line in islice(file, start_at - 1, end_at):
-            yield f"{line}\n" if line[-1] != "\n" else line
-
-
 @dataclass
-class Embed:
+class Embed(Iterable):
     file_path: Path
     start_at: int
     end_at: Optional[int]
@@ -29,19 +20,26 @@ class Embed:
     @classmethod
     def parse_from_extra(cls, extra: str) -> Embed:
         try:
-            pattern = r"\s*(?P<file_path>.+\S)(?:\s*\[\s*(?P<start_at>\d+)\s*(?:-|:|,)?\s*(?P<end_at>\d*)?\s*\])"
-            file_path, start_at, end_at = match(pattern, extra).group("file_path", "start_at", "end_at")
+            pattern = r"\s*(?P<file_path>.+\S)(?:\s*\[\s*(?P<start_at>\d*)\s*(?P<has_colon>:)?\s*(?P<end_at>\d*)?\s*\])"
+            file_path, start_at, has_colon, end_at = match(pattern, extra).group("file_path", "start_at", "has_colon", "end_at")
+            end_at = None if has_colon and not end_at else end_at
+            end_at = start_at if not has_colon and start_at else end_at
         except AttributeError:
             file_path, start_at, end_at = extra, 1, None
 
         return cls(
             file_path=Path(file_path),
-            start_at=int(start_at) or 1,
-            end_at=int(end_at) if end_at else None,
+            start_at=int(start_at or 1) or 1,
+            end_at=int(end_at) or 1 if end_at else None,
         )
 
+    def __iter__(self):
+        with self.file_path.open() as file:
+            for line in islice(file, self.start_at - 1, self.end_at):
+                yield f"{line}\n" if line[-1] != "\n" else line
+
     def __str__(self) -> str:
-        return ''.join(file_slice(**self.__dict__))
+        return ''.join(self)
 
 
 class MarkdownEmbedCodeRenderer(MarkdownRenderer):
@@ -62,13 +60,9 @@ class MarkdownEmbedCodeRenderer(MarkdownRenderer):
 _markdown = Markdown(renderer=MarkdownEmbedCodeRenderer)
 
 
-def render_markdown(document: str) -> str:
-    return _markdown(document)
-
-
 def render_markdown_file(file_path: Path):
     with file_path.open("r+") as file:
-        rendered_contents = render_markdown(file.read())
+        rendered_contents = _markdown(file.read())
         file.seek(0)
         file.write(rendered_contents)
         file.truncate()
